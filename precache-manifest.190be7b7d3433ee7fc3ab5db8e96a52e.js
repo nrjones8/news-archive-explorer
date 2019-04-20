1ce55cd67f65ecb504ea2f712d80b9151907bd15
Nick Jones <nrjones8@gmail.com>
2019-04-20 14:45:17 -0400

    Updates

---
+++ precache-manifest.190be7b7d3433ee7fc3ab5db8e96a52e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e05d6874be4291b7772b",
+    "url": "/news-archive-explorer/static/js/runtime~main.fac48073.js"
+  },
+  {
+    "revision": "3fca67a5c63293f98ba0",
+    "url": "/news-archive-explorer/static/js/main.24e1e29a.chunk.js"
+  },
+  {
+    "revision": "22e9f794882bf92e1f51",
+    "url": "/news-archive-explorer/static/js/2.0a0465b0.chunk.js"
+  },
+  {
+    "revision": "3fca67a5c63293f98ba0",
+    "url": "/news-archive-explorer/static/css/main.bbabaabd.chunk.css"
+  },
+  {
+    "revision": "22e9f794882bf92e1f51",
+    "url": "/news-archive-explorer/static/css/2.c5d0130d.chunk.css"
+  },
+  {
+    "revision": "54764c496dc8cb39305bb5bd7b0e2cc4",
+    "url": "/news-archive-explorer/index.html"
+  }
+];
