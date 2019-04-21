818d51152c8ea09ddfc6c8918343eababcd9ca18
Nick Jones <nrjones8@gmail.com>
2019-04-21 11:24:46 -0400

    Updates

--- precache-manifest.6061bf7b7fe084d8d69c84dbc7fa64a7.js
+++ precache-manifest.6061bf7b7fe084d8d69c84dbc7fa64a7.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/news-archive-explorer/static/js/runtime~main.fac48073.js"
   },
   {
-    "revision": "72076a4157ed334a6b93",
-    "url": "/news-archive-explorer/static/js/main.97c20af1.chunk.js"
+    "revision": "97db6ef733510d4e5a68",
+    "url": "/news-archive-explorer/static/js/main.4ead653b.chunk.js"
   },
   {
     "revision": "01dcf99e7eddf44d8eae",
     "url": "/news-archive-explorer/static/js/2.85fbb1fa.chunk.js"
   },
   {
-    "revision": "72076a4157ed334a6b93",
+    "revision": "97db6ef733510d4e5a68",
     "url": "/news-archive-explorer/static/css/main.b2579cea.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/news-archive-explorer/static/css/2.c920d86e.chunk.css"
   },
   {
-    "revision": "62a86530909331db8773b40e33b9eaef",
+    "revision": "5a42f90e95c8a6f616502447694c4b14",
     "url": "/news-archive-explorer/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/news-archive-explorer/precache-manifest.0508f796ff0b9b680cc986dda94f293f.js"
+  "/news-archive-explorer/precache-manifest.6061bf7b7fe084d8d69c84dbc7fa64a7.js"
 );
 
 workbox.clientsClaim();
