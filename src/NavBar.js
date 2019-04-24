5695ff29ad58a4f18f89d7a3b92db72ca2465cb1
Nick Jones <nrjones8@gmail.com>
2019-04-24 08:34:21 -0400

    remove (toggle off?) toggler

--- src/NavBar.js
+++ src/NavBar.js
@@ -5,9 +5,6 @@ class NavBar extends Component {
     return (
       <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
         <a class="navbar-brand" href="#">News Homepage Archive</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
       </nav>
     )
   }
