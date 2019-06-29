badf7c8d39d7cbdd01ee68858dba6e985593d67a
Nick Jones <nrjones8@gmail.com>
2019-06-29 17:45:35 -0400

    working navbar, goin with the #hash

--- src/About.js
+++ src/About.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 
+import NavBar from './NavBar.js';
+
+import 'bootswatch/dist/litera/bootstrap.css';
+import 'react-datepicker/dist/react-datepicker.css';
+import './App.css';
+
 class About extends Component {
   render() {
-    return <h1>Why would anyone be interested in this</h1>
+    return (
+        <div>
+            <NavBar />
+            <h1>Why would anyone be interested in this</h1>
+        </div>
+    )
   }
 }
 export default About;
--- src/NavBar.js
+++ src/NavBar.js
@@ -4,7 +4,25 @@ class NavBar extends Component {
   render() {
     return (
       <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-        <div className="navbar-brand">News Homepage Archive</div>
+        <a className="navbar-brand" href={process.env.PUBLIC_URL + "/#/"}>News Homepage Archive</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#explorerNavBar" aria-controls="explorerNavBar" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="explorerNavBar">
+            <ul className="navbar-nav">
+                <li className="nav-item active">
+                    {/* There is probably a better way to do this */}
+                    <a className="nav-link" href={process.env.PUBLIC_URL + "/#/"}>Home</a>
+                </li>
+            </ul>
+            <ul className="navbar-nav">
+                <li className="nav-item active">
+                    {/* There is probably a better way to do this */}
+                    <a className="nav-link" href={process.env.PUBLIC_URL + "/#/about"}>About</a>
+                </li>
+            </ul>
+        </div>
       </nav>
     )
   }
